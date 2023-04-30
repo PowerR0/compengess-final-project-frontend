e26bdb011350139ddd30e811e4f06a528f270472
Power_R0 <monthawat123.wa@gmail.com>
2023-04-30 19:15:37 +0700

    update

--- script_item.js
+++ script_item.js
@@ -1,75 +1,7 @@
 // TODO #4.0: Change this IP address to EC2 instance public IP address when you are going to deploy this web application
-const backendIPAddress = "52.1.164.79:3000";
+const backendIPAddress = "127.0.0.1:3000";
 
 let itemsData, courses = [];
-// TODO #2.3: Send Get items ("GET") request to backend server and store the response in itemsData variable
-const getItemsFromDB = async () => {
-  const options = {
-    method: "GET",
-    credentials: "include"
-  }
-  await fetch(`http://${backendIPAddress}/items`, options)
-    .then((response) => response.json())
-    .then((data) => {
-      itemsData = data
-    })
-    .catch((error) => console.error(error));
-};
-
-// TODO #2.4: Show items in table (Sort itemsData variable based on created_date in ascending order)
-const showItemsInTable = (itemsData) => {
-  const table_body = document.getElementById("main-table-body");
-  table_body.innerHTML = "";
-
-  itemsData.sort((a, b) => {
-    return a.deadline - b.deadline;
-  })
-
-  itemsData.map((item) => {
-    table_body.innerHTML += `
-        <tr id="${item.item_id}">
-            <td>${item.course_name}</td>
-            <td>${item.title}</td>
-            <td>${item.deadline}</td>
-            <td>${item.score}</td>
-        </tr>
-        `;
-  });
-};
-
-// TODO #2.5: Send Add an item ("POST") request to backend server and update items in the table
-const addItem = async () => {
-  const item = document.getElementById("item-to-add").value;
-  const name = document.getElementById("name-to-add").value;
-  const price = document.getElementById("price-to-add").value;
-
-  console.log(
-    "This function should fetch 'add item' route from backend server and update items in the table."
-  );
-};
-
-// TODO 2.6: Send Delete an item ("DELETE") request to backend server and update items in the table
-const deleteItem = async (item_id) => {
-  console.log(
-    "This function should fetch 'delete item' route in backend server and update items in the table."
-  );
-};
-
-const getUserProfile = async () => {
-  const options = {
-    method: "GET",
-    credentials: "include",
-  };
-  await fetch(
-    `http://${backendIPAddress}/courseville/get_profile_info`,
-    options
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data.user);
-    })
-    .catch((error) => console.error(error));
-};
 
 const getCid = async () => {
   const options = {
@@ -143,6 +75,7 @@ const createAssignmentTable = async () => {
     `;
   }
 };
+
 const authorizeApplication = () => {
   window.location.href = `http://${backendIPAddress}/courseville/auth_app`;
 };
@@ -152,8 +85,6 @@ const logout = async () => {
 };
 
 document.addEventListener("DOMContentLoaded", async function (event) {
-  console.log("Getting items from database.");
-  await getItemsFromDB();
   console.log("Creating assignments table.");
   await getCid();
   await createAssignmentTable();
